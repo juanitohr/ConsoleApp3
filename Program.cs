2ad290d7520ba198dde31777354ba901681629b6
jmaria.herrero <jmaria.herrero@SADINA03>
2022-06-20 09:42:12 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo FELIZ");
+
+            Console.ReadKey(); //Para la ejecución pulsando una tecla
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
